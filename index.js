f23dd5f76960bcc33ab219bb3e02d035adb23013
Akanksha Singh <74486391+akankshasingh272@users.noreply.github.com>
2023-01-30 11:16:17 +0000

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ if (bmi<18.5) {
   if (bmi >= 18.5 && bmi <= 24.9) {
     console.log("Your BMI is "+bmi+ ", so you have a normal weight.");
   } else {
-    if (bmi < 24.9){
+    if (bmi > 24.9){
       console.log("Your BMI is "+bmi+ ", so you are overweight.");
     }
   }
